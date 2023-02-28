0207393ac5ed44d935b2d0ea6a21bf0c26d0e80c
shanku97 <94614497+shanku97@users.noreply.github.com>
2023-02-28 23:23:05 +0530

    Add files via upload
    error_file

---
+++ src/main/java/com/example/cmdAppTest/dto/ErrorDTO.java
@@ -0,0 +1,18 @@
+package com.example.cmdAppTest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class ErrorDTO {
+	
+	
+	private String message;
+
+}
