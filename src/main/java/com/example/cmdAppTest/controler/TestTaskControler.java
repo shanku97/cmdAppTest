fa3b6350609478ad8b52d514546480dc61df1178
shanku97 <94614497+shanku97@users.noreply.github.com>
2023-02-28 23:28:58 +0530

    Update TestTaskControler.java
    ExceptionHandler

--- src/main/java/com/example/cmdAppTest/controler/TestTaskControler.java
+++ src/main/java/com/example/cmdAppTest/controler/TestTaskControler.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ParseException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cmdAppTest.dto.CreateTaskDTO;
+import com.example.cmdAppTest.dto.ErrorDTO;
 import com.example.cmdAppTest.entity.TestTaskEntity;
 import com.example.cmdAppTest.service.TestTaskService;
-import com.example.dto.CreateTaskDTO;
+//import com.example.dto.CreateTaskDTO;
 
 @RestController
 @RequestMapping("/tasks")
@@ -21,8 +26,8 @@ public class TestTaskControler {
 	
 	@Autowired
 	TestTaskService testTaskService;
-	@Autowired
-	CreateTaskDTO createTaskDTO;
+//	@Autowired
+//	CreateTaskDTO createTaskDTO;
 
 	@GetMapping("")
 	public ResponseEntity<List<TestTaskEntity>> getTasks(){
@@ -31,8 +36,8 @@ public class TestTaskControler {
 		return ResponseEntity.ok(tasks);
 	}
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<TestTaskEntity> getTaskById(int id){
+	@GetMapping("/byId{id}")
+	public ResponseEntity<TestTaskEntity> getTaskById(@PathVariable("id")Integer id){
 		var task = testTaskService.getTaskById(id);
 		if(task==null) {
 			return ResponseEntity.notFound().build();
@@ -40,11 +45,21 @@ public class TestTaskControler {
 		return ResponseEntity.ok(task);
 	}
 	
-	@PostMapping("")
-	public ResponseEntity<TestTaskEntity> addTask(@RequestBody CreateTaskDTO body){
+	@PostMapping("/addTask")
+	public ResponseEntity<TestTaskEntity> addTask(@RequestBody CreateTaskDTO body) throws java.text.ParseException{
 		
 		var task = testTaskService.addTask(body.getTitle(), body.getDescription(), body.getDeadline());
 		
 		return ResponseEntity.ok(task);
 	}
+	
+	@ExceptionHandler(ParseException.class)
+	public ResponseEntity<ErrorDTO> handleErrors(Exception e){
+		if(e instanceof ParseException) {
+			return ResponseEntity.badRequest().body(new ErrorDTO("Invalid date format"));
+		}
+		return ResponseEntity.internalServerError().body(new ErrorDTO("server error"));
+		
+	}
 }
+
